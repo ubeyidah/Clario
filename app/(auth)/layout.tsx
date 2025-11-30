fadcb48a9667eb94e1de6746fd51f58a5cb05b81
ubeyidah <ubeyidah@gmail.com>
2025-11-30 22:20:22 +0300

    feat: implement dark mode

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <div className="flex items-center justify-center min-h-svh">
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      {children}
+    </div>
+  </div>
+}
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
 
 export default function SignIn() {
 
-  return <div>sign in today
-    <Button>hello</Button></div>
+  return <Card>
+    <CardHeader>
+      <CardTitle>Welcome Back!</CardTitle>
+      <CardDescription>Login with yout Github or Email address</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <Button className="w-full">
+        <GithubIcon className="size-4" />
+        Sign in with Github</Button>
+    </CardContent>
+  </Card>
 }
