58ed5772ca58185f7b267563c4538729c9230725
ubeyidah <ubeyidah@gmail.com>
2025-11-30 23:09:35 +0300

    feat: design sign in page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return <div className="flex items-center justify-center min-h-svh">
+    <Link href={"/"} className={buttonVariants({ variant: "outline", className: "absolute top-5 left-5 hover:gap-5" })}>
+      <ArrowLeft /> Back
+    </Link>
     <div className="flex w-full max-w-sm flex-col gap-6">
+      <Link href={"/"} className="flex items-center flex-col self-center font-medium">
+        <Image src={"/clario.png"} width={60} height={60} alt="clario logo" />
+        Clario.
+      </Link>
       {children}
+      <div className="text-balance text-xs text-muted-foreground text-center">
+        By clicking continue, you agree to our <span className="hover:text-primary hover:underline">Terms of service</span> and <span className="hover:text-primary hover:underline">Privacy Policy</span>.
+      </div>
     </div>
   </div>
 }
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function SignIn() {
@@ -9,10 +11,20 @@ export default function SignIn() {
       <CardTitle>Welcome Back!</CardTitle>
       <CardDescription>Login with yout Github or Email address</CardDescription>
     </CardHeader>
-    <CardContent>
+    <CardContent className="flex flex-col gap-4">
       <Button className="w-full">
         <GithubIcon className="size-4" />
         Sign in with Github</Button>
+      <div className="text-center relative text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:border-t after:border-border after:flex after:items-center">
+        <span className="relative z-10 bg-card px-2 text-muted-foreground">Or continue with</span>
+      </div>
+      <div className="grid gap-3">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="Email Address" />
+        </div>
+        <Button>Continue with Email</Button>
+      </div>
     </CardContent>
   </Card>
 }
