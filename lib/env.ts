0a8baf6b8d0d109f578a08c559cade34e7572d79
ubeyidah <ubeyidah@gmail.com>
2025-12-07 12:29:26 +0300

    feat: log otp to console if it is under dev

--- lib/env.ts
+++ lib/env.ts
@@ -4,5 +4,6 @@ export const env = defineEnv({
   DATABASE_URL: z.url(),
   GITHUB_CLIENT_ID: z.string().min(1),
   GITHUB_CLIENT_SECRET: z.string().min(1),
-  RESEND_API_KEY: z.string().startsWith("re_")
+  RESEND_API_KEY: z.string().startsWith("re_").optional(),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 })
--- lib/resend.ts
+++ lib/resend.ts
@@ -5,12 +5,16 @@ import { otpEmailTemplate } from './email/templates/otp-template';
 const resend = new Resend(env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (email: string, otp: string) => {
-    const name = email.split("@")[0];
-    await resend.emails.send({
-        from: 'Clario LMS <onboarding@resend.dev>',
-        to: [email],
-        subject: "Your Clario LMS Sign-In Code",
-        html: otpEmailTemplate({ otp, userName: name }),
-    });
+  if (env.NODE_ENV === "development") {
+    console.log(`[DEV] OTP for ${email}: ${otp}`);
+    return; // skip sending real email
+  }
+  const name = email.split("@")[0];
+  await resend.emails.send({
+    from: 'Clario LMS <onboarding@resend.dev>',
+    to: [email],
+    subject: "Your Clario LMS Sign-In Code",
+    html: otpEmailTemplate({ otp, userName: name }),
+  });
 
 }
