f0a0ca1b6af71e612686be4cc4138cd5cf7dfb20
ubeyidah <ubeyidah@gmail.com>
2025-12-13 15:54:54 +0300

    feat: connect links to there according page

--- app/admin/courses/_components/course-card.tsx
+++ app/admin/courses/_components/course-card.tsx
@@ -1,12 +1,13 @@
 import { AdminCoursesType } from "@/app/data/admin/get-admin-courses"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import { useFileKeyToUrl } from "@/hooks/use-file-key-to-url"
 import { badgeClasses, courseStatusToVariant } from "@/lib/badge"
 import { cn, formatDuration, formatETB } from "@/lib/utils"
-import { Edit, Eye, Trash } from "lucide-react"
+import { Edit, Eye } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 
 
 interface iAppProps {
@@ -18,23 +19,25 @@ interface iAppProps {
 const CourseCard = ({ course, isFirst }: iAppProps) => {
   const thumbnailUrl = useFileKeyToUrl(course.fileKey)
   return (
-    <div className={cn("border-b group py-4 px-2 hover:bg-card/50 transition-colors duration-300 grid grid-cols-[200px_1fr] gap-4", isFirst && "border-t")}>
+    <div className={cn("border-b group py-4 px-2 hover:bg-card/30 transition-colors duration-300 grid grid-cols-[200px_1fr] gap-4", isFirst && "border-t")}>
       <div className="relative rounded-xl bg-linear-to-bl from-neutral-100/30 via-neutral-50/30 to-neutral-100/30 overflow-hidden">
-        <Image src={thumbnailUrl} width={200} height={100} alt="thumbnail image" className="aspect-video rounded-xl" loading="lazy" />
+        <Link href={`/admin/courses/${course.id}`}>
+          <Image src={thumbnailUrl} width={200} height={100} alt="thumbnail image" className="aspect-video rounded-xl" loading="lazy" />
+        </Link>
         <p className="absolute bottom-2 px-2 py-0.5 rounded-md right-2 text-sm bg-background/50 backdrop-blur-3xl">{formatDuration(course.duration)}</p>
       </div>
       <div className="flex flex-col justify-between relative">
         <div className={badgeClasses(courseStatusToVariant(course.status), "absolute top-0 right-2")}>{course.status}</div>
         <div>
-          <h1 className="text-lg font-bold">{course.title}</h1>
+          <Link href={`/admin/courses/${course.id}`} className="text-lg font-bold hover:underline">{course.title}</Link>
           <p className="text-muted-foreground line-clamp-2">{course.shortDescription}</p>
           <div className="absolute bottom-2 right-2 gap-2 flex opacity-0 translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
             <TooltipProvider>
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <Button variant={"ghost"} size={"sm"} className="cursor-pointer rounded-full">
+                  <Link href={`/admin/courses/${course.id}/edit`} className={buttonVariants({ variant: "ghost", size: "sm", className: "cursor-pointer rounded-full" })}>
                     <Edit />
-                  </Button>
+                  </Link>
                 </TooltipTrigger>
                 <TooltipContent>
                   Edit
@@ -43,23 +46,12 @@ const CourseCard = ({ course, isFirst }: iAppProps) => {
 
               <Tooltip>
                 <TooltipTrigger asChild>
-                  <Button variant={"ghost"} size={"sm"} className="cursor-pointer rounded-full">
+                  <Link href={`/admin/courses/${course.id}`} className={buttonVariants({ variant: "ghost", size: "sm", className: "cursor-pointer rounded-full" })}>
                     <Eye />
-                  </Button>
+                  </Link>
                 </TooltipTrigger>
                 <TooltipContent>
                   View Detail
-                </TooltipContent>
-              </Tooltip>
-
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Button variant={"ghost"} size={"sm"} className="cursor-pointer text-destructive hover:bg-destructive/20 hover:text-destructive rounded-full">
-                    <Trash />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                  Delete
                 </TooltipContent>
               </Tooltip>
 
