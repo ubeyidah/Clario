269935ca9df89996ef4cbd530350478ebb78a578
ubeyidah <ubeyidah@gmail.com>
2025-12-20 15:43:14 +0300

    feat: add search and filter functionality to admin courses page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -6,16 +6,14 @@ import CourseCardSkeleton from "./_components/course-card-skeleton";
 import { CoursesFilters } from "./_components/courses-filters";
 import { CoursesList } from "./_components/courses-list";
 
-interface CoursesPageProps {
-  searchParams: {
-    search?: string;
-    category?: string;
-    status?: string;
-  };
-}
+type SearchParams = {
+  search?: string;
+  category?: string;
+  status?: string;
+};
 
-const CoursesPage = async ({ searchParams }: CoursesPageProps) => {
-  const { search, category, status } = searchParams;
+const CoursesPage = async ({ searchParams }: { searchParams: Promise<SearchParams> }) => {
+  const { search, category, status } = await searchParams;
 
   return (
     <main>
@@ -25,7 +23,7 @@ const CoursesPage = async ({ searchParams }: CoursesPageProps) => {
       <div className="p-4 md:p-6">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
           <h1 className="text-xl font-sem">Your Courses</h1>
-          <CoursesFilters 
+          <CoursesFilters
             initialSearch={search}
             initialCategory={category}
             initialStatus={status}
@@ -33,7 +31,7 @@ const CoursesPage = async ({ searchParams }: CoursesPageProps) => {
         </div>
 
         <Suspense fallback={<CourseCardSkeleton />}>
-          <CoursesList 
+          <CoursesList
             searchQuery={search}
             category={category}
             status={status}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,15 +31,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster />
-          {children}
-        </ThemeProvider>
+        <NuqsAdapter>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
